505015362ca4151736679f506d8dfa86ca80bda3
Dawson Riggs <dawsoncriggs@gmail.com>
2024-06-26 08:52:46 -0500

    Updated token contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
@@ -12,11 +12,7 @@ contract Token {
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
-    event Transfer(
-        address indexed from,
-        address indexed to,
-        uint256 value
-    );
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     event Approval(
         address indexed owner,
@@ -31,40 +27,33 @@ contract Token {
     ) {
         name = _name;
         symbol = _symbol;
-        totalSupply = _totalSupply * (10**decimals);
+        totalSupply = _totalSupply * (10 ** decimals);
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address _to, uint256 _value)
-        public
-        returns (bool success)
-    {
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
-
         _transfer(msg.sender, _to, _value);
-
         return true;
     }
 
-    function _transfer(
-        address _from,
-        address _to,
-        uint256 _value
-    ) internal {
+    function _transfer(address _from, address _to, uint256 _value) internal {
         require(_to != address(0));
 
-        balanceOf[_from] = balanceOf[_from] - _value;
-        balanceOf[_to] = balanceOf[_to] + _value;
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
 
         emit Transfer(_from, _to, _value);
     }
 
-    function approve(address _spender, uint256 _value)
-        public
-        returns(bool success)
-    {
+    function approve(
+        address _spender,
+        uint256 _value
+    ) public returns (bool success) {
         require(_spender != address(0));
-
         allowance[msg.sender][_spender] = _value;
 
         emit Approval(msg.sender, _spender, _value);
@@ -75,18 +64,13 @@ contract Token {
         address _from,
         address _to,
         uint256 _value
-    )
-        public
-        returns (bool success)
-    {
-        require(_value <= balanceOf[_from]);
-        require(_value <= allowance[_from][msg.sender]);
+    ) public returns (bool success) {
+        require(balanceOf[_from] >= _value);
+        require(allowance[_from][msg.sender] >= _value);
 
-        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+        allowance[_from][msg.sender] -= _value;
 
         _transfer(_from, _to, _value);
-
         return true;
     }
-
 }
