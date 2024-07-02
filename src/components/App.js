37ada995c101b30a124180f5abdf1d90895d06ea
Dawson Riggs <dawsoncriggs@gmail.com>
2024-07-02 18:55:14 -0500

    List proposals

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import { ethers } from "ethers";
 // Components
 import Navigation from "./Navigation";
 import Loading from "./Loading";
+import Proposals from "./Proposals";
 
 // ABIs: Import your contract ABIs here
 import DAO_ABI from "../abis/DAO.json";
@@ -13,15 +14,18 @@ import DAO_ABI from "../abis/DAO.json";
 import config from "../config.json";
 
 function App() {
-  const [account, setAccount] = useState(null);
+  const [provider, setProvider] = useState(null);
   const [dao, setDao] = useState(null);
   const [treasuryBalance, setTreasuryBalance] = useState(null);
-
+  const [account, setAccount] = useState(null);
+  const [proposals, setProposals] = useState(null);
+  const [quorum, setQuorum] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
   const loadBlockchainData = async () => {
     // Initiate provider
     const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
 
     // Initiate contracts
     const dao = new ethers.Contract(
@@ -42,6 +46,18 @@ function App() {
     const account = ethers.utils.getAddress(accounts[0]);
     setAccount(account);
 
+    const count = await dao.proposalCount();
+    const items = [];
+
+    for (var i = 0; i < count; i++) {
+      const proposal = await dao.proposals(i + 1);
+      items.push(proposal);
+    }
+
+    setProposals(items);
+
+    setQuorum(await dao.quorum());
+
     setIsLoading(false);
   };
 
@@ -61,9 +77,18 @@ function App() {
         <Loading />
       ) : (
         <>
-          <hr/>
-          <p className="text-center"><strong>Treasury Balance:</strong> {treasuryBalance} ETH</p>
-          <hr/>
+          <hr />
+          <p className="text-center">
+            <strong>Treasury Balance:</strong> {treasuryBalance} ETH
+          </p>
+          <hr />
+          <Proposals
+            provider={provider}
+            dao={dao}
+            proposals={proposals}
+            quorum={quorum}
+            setIsLoading={setIsLoading}
+          />
         </>
       )}
     </Container>
