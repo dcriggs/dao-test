608a1388a02f50650ee1832589afa3b4c1aafe60
Dawson Riggs <dawsoncriggs@gmail.com>
2024-09-26 18:33:02 -0500

    Update branding for deployment

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,8 +8,8 @@ const Navigation = ({ account }) => {
       <img
         alt="logo"
         src={logo}
-        width="40"
-        height="40"
+        width="120"
+        height="120"
         className="d-inline-block align-top mx-3"
       />
       <Navbar.Brand href="#">Dawson's DAO</Navbar.Brand>
